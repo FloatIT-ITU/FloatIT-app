58b9ff16e83e0225c48c5884a59bfa482f03e445
Pedro Prazeres <pedronprazeres@gmail.com>
2025-10-25 22:42:05 +0200

    web: Update FCM service worker to Firebase JS SDK v10.7.0

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging.js');
 
 firebase.initializeApp({
   apiKey: 'AIzaSyAC7B4DLfnCqr292V0ulvJINss0Wzsvfnw',
